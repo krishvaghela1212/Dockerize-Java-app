73c835d615afa40e65f249bc919ad62839f67a15
KrishVaghela <KrishVaghela123>
2025-05-26 16:40:51 +0530

    Initial Commit

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class main {
+  public static void main(String[] args) {
+    Date currentDate = new Date();
+    System.out.println("Hello Docker !! , CurrentDate is : " + currentDate);
+
+  }
+
+}
